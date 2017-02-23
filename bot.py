7dfcc8bec7dce7187d88a83cfa5ed6f7336bb1bf
unnar <unnar93@gmail.com>
2017-02-23 14:11:39 +0000

    It now checks if it has already reported a comment/submission before doing so.

--- bot.py
+++ bot.py
@@ -7,6 +7,27 @@ reddit = praw.Reddit('findareddit')
 
 subreddit = reddit.subreddit('findareddit')
 
+#List of reported submissions
+reported_submissions = []
+#List of reported comments
+reported_comments = []
+
+try:
+    with open('reported_submissions.txt', 'r') as f:
+        for line in f:
+            reported_submissions.append(line)
+except FileNotFoundError:
+    with open('reported_submissions.txt', 'w') as f:
+        pass
+
+try:
+    with open('reported_comments.txt', 'r') as f:
+        for line in f:
+            reported_comments.append(line)
+except FileNotFoundError:
+    with open('reported_comments.txt', 'w') as f:
+        pass
+
 print("Going through submissions")
 #Check if any submission is under the limit
 for submission in subreddit.new(limit=100):
@@ -14,26 +35,39 @@ for submission in subreddit.new(limit=100):
         #Submission is more than 24 hours old, no need to look further
         continue
     elif submission.score <= -3:
-        submission.report("Submission has a score of {}.".format(submission.score))
         print("-------------------")
         print("Found post with score of negative 3 or less.")
         print("Title: ", submission.title)
         print("-------------------")
+        if submission.id in reported_submissions:
+            print("Submission has already been reported, continuing.")
+            continue    
+        submission.report("Submission has a score of {}.".format(submission.score))
+        with open('reported_submissions.txt', 'a') as f:
+            f.write(submission.id+'\n')
+        print("Submission has been reported and written to file.")
+
 
 print("Finished going through submissions, now sleeping before going through comments")
 time.sleep(5)
 print("Now going through comments")
-for i, comment in subreddit.comments(limit=100):
+for comment in subreddit.comments(limit=100):
     if datetime.utcnow() > (datetime.utcfromtimestamp(comment.created_utc) + relativedelta(hours=24)):
         #Comment is more than 24 hours old, no need to look further
         break
     if comment.score <= -3:
-        comment.report("Comment has a score of {}.".format(comment.score))
         print("-------------------")
         print("Found comment with score of negative 3 or less.")
         print("Comment ID: ", comment.id)
         print("Comment body: ",comment.body)
         print("-------------------")
+        if comment.id in reported_comments:
+            print("Comment has already been reported, continuing.")
+            continue
+        comment.report("Comment has a score of {}.".format(comment.score))
+        with open('reported_comments.txt', 'a') as f:
+            f.write(comment.id+'\n')
+        print("Comment has been reported and written to file.")
 print("Finished going through all comments")
 
 
